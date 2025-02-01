c233aee91d4617bc64a933aa992b121dc4198d8f
Junaid <junaidislam441@gmail.com>
2025-02-01 23:25:57 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Greet from './Components/Greet.jsx'
+
+const App = () => {
+  return (
+   <>
+   <Greet name="Junaid" />
+   </>
+  )
+}
+
+export default App
---
+++ src/Components/Book.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Book = () => {
+  return (
+    <div>
+        <h1>Clean Code</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus sit doloremque sunt ullam dicta beatae quis quaerat quasi maxime magnam quidem adipisci facere deserunt provident modi, molestiae quisquam ea accusantium.</p>
+        <ul>
+            <li>1. molestiae quisquam ea</li>
+            <li>2. molestiae quisquam ea</li>
+            <li>3. molestiae quisquam ea</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Book
